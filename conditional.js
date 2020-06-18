e50a586eefc04f29969abd49a2a3b7b4b375999f
Iman Mohi <imanmohi786@gmail.com>
2020-06-19 00:40:36 +0530

    more prac

---
+++ conditional.js
@@ -0,0 +1,29 @@
+switch (userChoice) {
+    case 'pizza':
+        console.log('$1.50');
+        break;
+    case 'spaghetti':
+        console.log('$5.50');
+        break;
+    case 'lasagna':
+        console.log('$8.50');
+        break;
+    case 'hamburger':
+        console.log('$2.50');
+        break;
+    case 'cheeseburger':
+        console.log('$3.00');
+        break;
+    case 'chicken tenders':
+        console.log('$3.50');
+        break;
+    case 'taco':
+        console.log('$2.00');
+        break;
+    case 'burrito':
+        console.log('$4.00');
+        break;
+    default:
+        console.log('Please select an option');
+
+}
---
+++ operator.js
@@ -0,0 +1,18 @@
+var foo = ['one', 'two', 'three'];
+
+//without destructuring
+var one = foo[0];
+var two = foo[1];
+var three = foo[2];
+
+console.log(one);
+console.log(two);
+console.log(three);
+console.log([one, two, three]);
+
+//with destructuring
+var[one, two, three] = foo;
+console.log(one);
+console.log(two);
+console.log(three);
+console.log([one, two, three]);
